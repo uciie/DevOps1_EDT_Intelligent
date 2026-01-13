529a715131417354a5d2e6c2d02e1aa68a4c3617
Lucie <pan.luciie@gmail.com>
2026-01-13 09:54:57 +0100

    refacto

--- backend/src/test/java/com/example/backend/service/impl/TaskServiceImplTest.java
+++ backend/src/test/java/com/example/backend/service/impl/TaskServiceImplTest.java
@@ -16,7 +16,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Collections;
